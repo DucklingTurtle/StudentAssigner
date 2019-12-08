e835a6392973bc0434196ac0b815048afb45182e
DucklingTurtle <howtech1234@gmail.com>
2019-12-08 12:31:51 -0800

    Added see student stats command

--- __init__.py
+++ __init__.py
@@ -2,11 +2,12 @@ import classes
 
 commands_list = "Student Input, Teacher Input, "
 commands_student = "Create Student, See Students, Remove Student"
-commands_student_stats = "See Student Stats, "
+commands_student_stats = "See Student Stats, Back"
 commands_teacher = "Create Teacher, See Teachers, Remove Teacher"
 mode = "init"
 students_list = [
-    {"Name": "Test_name", "Student 1": classes.Student("Test_Name", "Test_Focus1", "Test_Focus2")}
+    {"Name": "Test_name", "Test_name": classes.Student("Test_Name", "Test_Focus1", "Test_Focus2")},
+    {"Name": "Test_name2", "Object": classes.Student("Test_Name2", "Test_Focus1", "Test_Focus2")}
 ]
 teachers_list = []
 advisers = []
@@ -20,6 +21,7 @@ def commands(command):
     elif command == "teacher input":
         mode = "teacher"
 
+# runs all commands
 while True:
     if mode == "init":
         while mode == "init":
@@ -28,25 +30,37 @@ while True:
     elif mode == "student":
         while mode == "student":
             print("\nAvailable Commands: " + commands_student + "\nEnter Command: ")
-            user_input = input("")
+            user_input = input("").lower()
             if user_input == "create student":
                 student_name = input("Student's name?: \n")
                 student_focus1 = input("Student's first area of focus?: \n")
                 student_focus2 = input("Student's second area of focus?: \n")
                 students_list.update({"Name": student_name, student_name : classes.Student(student_name, student_focus1, student_focus2)})
             elif user_input == "see students":
+                print("Students: ")
                 for index in range(len(students_list)):
-                    print("Students: ")
                     print(students_list[index]["Name"])
-                # see
-                print("\nAvailable Commands: " + commands_student_stats + "\nEnter Command: ")
-                user_input = input("")
-                if user_input == "See Student Stats":
-                    print("What student?: ")
-                    choosen_student = input("")
-                    # for index in students_list:
-                        # if choose_student == list(students_list.keys())[index]:
-                        #     print("True")
+                # commands under see students
+                while True:
+                    print("\nAvailable Commands: " + commands_student_stats + "\nEnter Command: ")
+                    user_input = input("").lower()
+                    if user_input == "see student stats":
+                        print("What student?: ")
+                        choosen_student = input("")
+                        for index in range(len(students_list)):
+                            if choosen_student == students_list[index][choosen_student]:
+                                print("Succesful")
+                                print("Name: " + students_list[index][choosen_student].name)
+                                      # + "\nFocus Area 1: " + students_list[index][choosen_student].focus1)
+                                      # "\nFocus 1 Teacher: " + students_list[index][choosen_student].focus1_teachers +
+                                      # "\nFocus Area 2: " + students_list[index][choosen_student].focus2 +
+                                      # "\nFocus Area 2 Teacher: " + students_list[index][choosen_student].focus2_teachers +
+                                      # "\nAdviser: " + students_list[index][choosen_student].adviser)
+                                # print("Focus Area 1: " + students_list[index][choosen_student].focus1)
+                    elif user_input == "back":
+                        break
+                    else:
+                        print("Invalid Command")
 
 
     elif mode == "teacher":
--- classes.py
+++ classes.py
@@ -1,7 +1,7 @@
 class Student:
     def __init__(self, name, focus1, focus2):
         self.name = name
-        self.adviser = None
+        self.adviser = "Not applied yet"
         self.focus1 = focus1
         self.focus1_teachers = []
         self.focus2 = focus2
