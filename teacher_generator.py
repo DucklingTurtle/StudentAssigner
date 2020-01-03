de105c196342b85292af3d2208be9dfa41331eb3
DucklingTurtle <howtech1234@gmail.com>
2020-01-02 19:08:39 -0800

    Teacher generator fully working

--- teacher_generator.py
+++ teacher_generator.py
@@ -14,11 +14,12 @@ gc = gspread.authorize(creds)
 sh = gc.open("Teacher Input 2 Sheet")
 ws = sh.get_worksheet(0)
 name_num = 0
-for i in range(50):
-    i += 2
+for x in range(50):
+    days_pool = []
+    x += 2
     # create random values
     name_num += 1
-    name = "First" + str(name_num) + "" + "Last" + str(name_num)
+    name = "First" + str(name_num) + " " + "Last" + str(name_num)
     focus1 = random.choice(focus_list)
     focus2 = random.choice(focus_list)
     while True:
@@ -27,23 +28,28 @@ for i in range(50):
         else:
             break
     # name
-    ws.update_cell(i, 2, name)
+    ws.update_cell(x, 2, name)
     # focus 1
-    ws.update_cell(i, 3, focus1)
+    ws.update_cell(x, 3, focus1)
     # focus 2
-    ws.update_cell(i, 4, focus2)
+    ws.update_cell(x, 4, focus2)
     # number of days
     days_available_list = []
     num_of_days = random.choice(range(1, 11))
-    for element in range(num_of_days + 1):
+    print("num_of_days: " + str(num_of_days))
+    for element in range(num_of_days):
+        rand_day = random.choice(range(1, 11))
         while True:
-            rand_day = random.choice(range(1, 11))
             if not any(x == rand_day for x in days_available_list):
                 days_available_list.append(rand_day)
                 break
-    days_available = str(days_available_list[1:-1])
+            else:
+                rand_day = random.choice(range(1, 11))
+                print("rand_day: " + str(rand_day))
+    days_available = str(days_available_list)[1:-1]
     # days available
-    ws.update_cell(i, 5, days_available)
+    print("days_available: " + days_available)
+    ws.update_cell(x, 5, days_available)
     # hours for days
     # hours_dict = {}
     cell = 5
@@ -52,8 +58,6 @@ for i in range(50):
         hours = []
         cell += 1
         for number in range(num_hours):
-            print("works")
-            print(number)
             choosen_hour = random.choice(range(1, 4))
             while True:
                 if any(x == choosen_hour for x in hours):
@@ -68,6 +72,12 @@ for i in range(50):
                     hours.append(choosen_hour)
                     break
         day_hours = ",".join(hours)
-        ws.update_cell(i, cell, day_hours)
-        time.sleep(5)
+        days_pool = days_available_list
+        for e in days_pool:
+            for i in range(1, 11):
+                if e == i:
+                    days_pool.remove(e)
+                    index = i + 5
+                    ws.update_cell(x, index, day_hours)
+        time.sleep(1)
     time.sleep(10)
